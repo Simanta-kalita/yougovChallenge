12a9c0ed2b9592446b0e3f71b9bf6fd54dc66d30
Simanta-kalita <58041419+Simanta-kalita@users.noreply.github.com>
2022-06-27 17:01:48 +0530

    Implemened UI of Change Vote

--- src/poller/Vote.svelte
+++ src/poller/Vote.svelte
@@ -1,62 +1,73 @@
 <script lang="ts">
-  import { votesDao } from "./votesDao";
-  import { pollsDao } from "./pollsDao";
-  import {
-    TileGroup,
-    RadioTile,
-    Button,
-    Tile,
-    Loading,
-  } from "carbon-components-svelte";
-  import type { Vote } from "./Vote";
-  import ErrorToast from "./ErrorToast.svelte";
+ import { votesDao } from "./votesDao";
+ import { pollsDao } from "./pollsDao";
+ import {
+   TileGroup,
+   RadioTile,
+   Button,
+   Tile,
+   Loading,
+ } from "carbon-components-svelte";
+ import type { Vote } from "./Vote";
+ import ErrorToast from "./ErrorToast.svelte";
 import type { VoteDao } from "./VoteDao";
-
-  export let pollId: string;
-  let selected: string;
-  let submittedVote: Promise<Vote | undefined>;
-  
+ 
+ export let pollId: string;
+ let selected: string;
+ let submittedVote: Promise<Vote | undefined>;
+ let selectedVote: boolean;
   async function loadVote(dao: VoteDao, id: string): Promise<Vote | undefined> {
-    const vote = await dao.getForPoll(id)
-    selected = vote ? `${vote.choice}` : selected
-    return vote;
-  }
+   const vote = await dao.getForPoll(id)
+   selected = vote ? `${vote.choice}` : selected
+   selectedVote = vote ? false : true
+   return vote;
+ }
 </script>
-
+ 
 {#await $pollsDao.getValue({ id: pollId })}
-  <Loading />
+ <Loading />
 {:then poll}
-  {#await loadVote($votesDao, pollId)}
-    <Loading />
-  {:then previousVote}
-    <Tile>
-      <h4>{poll.question}</h4>
-    </Tile>
-    <Tile>
-      <TileGroup bind:selected>
-        {#each poll.answers as answer, index}
-          <RadioTile
-            value={index + ""}
-            disabled={!!previousVote || !!submittedVote}>{answer}</RadioTile
-          >
-        {/each}
-      </TileGroup>
-    </Tile>
-    <Tile>
-      <Button
-        on:click={() =>
-          (submittedVote = $votesDao.vote({
-            choice: parseInt(selected),
-            pollId: poll.id,
-          }))}
-        disabled={!selected || !!previousVote || !!submittedVote}
-      >
-        Vote
-      </Button>
-    </Tile>
-  {:catch error}
-    <ErrorToast {error} />
-  {/await}
+ {#await loadVote($votesDao, pollId)}
+   <Loading />
+ {:then previousVote}
+   <Tile>
+     <h4>{poll.question}</h4>
+   </Tile>
+   <Tile>
+     <TileGroup bind:selected>
+       {#each poll.answers as answer, index}
+         <RadioTile
+           value={index + ""}
+           disabled={(!!previousVote || !!submittedVote) && !selectedVote}>{answer}</RadioTile
+         >
+       {/each}
+     </TileGroup>
+   </Tile>
+   <Tile>
+     <Button
+       on:click={() =>
+         (submittedVote = $votesDao.vote({
+           choice: parseInt(selected),
+           pollId: poll.id,
+         }))}
+       on:click={() =>
+         (selectedVote = false)}
+       disabled={(!selected || !!previousVote || !!submittedVote) && !selectedVote}
+     >
+       Vote
+     </Button>
+   </Tile>
+   <Tile>
+     <Button
+       on:click={() =>
+         (selectedVote = true)}
+     >
+       Change Vote
+     </Button>
+   </Tile>
+ {:catch error}
+   <ErrorToast {error} />
+ {/await}
 {:catch error}
-  <ErrorToast {error} />
+ <ErrorToast {error} />
 {/await}
